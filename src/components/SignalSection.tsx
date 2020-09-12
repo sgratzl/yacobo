052d980123b71c94ff2fae974438eb72387800d9
Samuel Gratzl <sam@sgratzl.com>
2020-09-12 05:02:43 +0200

    format code

--- src/components/SignalSection.tsx
+++ src/components/SignalSection.tsx
@@ -1,20 +1,21 @@
-import useSWR from "swr";
-import { fetchMeta } from "../data";
-import { ISignal } from "../data/constants";
+import useSWR from 'swr';
+import { fetchMeta } from '../data';
+import { ISignal } from '../data/constants';
 
 export default function SignalSection({ signal }: { signal: ISignal }) {
-
   const { data, error } = useSWR(`/api/meta`, fetchMeta);
 
-  if (error) return <div>Failed to load user</div>
-  if (!data) return <div>Loading...</div>
+  if (error) return <div>Failed to load user</div>;
+  if (!data) return <div>Loading...</div>;
   return (
     <section key={signal.id}>
       <header>
         <h2>{signal.label}</h2>
       </header>
       <p>{signal.description}</p>
-      <div>{signal.mean} +- {signal.stdev}</div>
+      <div>
+        {signal.mean} +- {signal.stdev}
+      </div>
       <div>{JSON.stringify(data)}</div>
     </section>
   );
