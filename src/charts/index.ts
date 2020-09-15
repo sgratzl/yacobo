c25875e74b5677dfd730ce8c530b80c4543fb16b
Samuel Gratzl <sam@sgratzl.com>
2020-09-15 16:58:14 +0200

    try adding font

--- src/charts/index.ts
+++ src/charts/index.ts
@@ -5,6 +5,14 @@ import { ISignal, ISignalMeta } from '../data/signals';
 import { LayerSpec, UnitSpec } from 'vega-lite/build/src/spec';
 import { startOfToday } from 'date-fns';
 import { DataSource } from 'vega-lite/build/src/data';
+import canvas from 'canvas';
+import { resolve } from 'path';
+
+canvas.registerFont(resolve('./fonts/Roboto-Regular.ttf'), {
+  family: 'Roboto',
+  style: 'normal',
+  weight: '400',
+});
 
 const font = `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`;
 
