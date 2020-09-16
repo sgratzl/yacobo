e0ce88ae7e6eb62e11cf5719df7e6c76e3ca8751
Samuel Gratzl <sam@sgratzl.com>
2020-09-16 19:44:13 +0200

    simplify font

--- src/charts/index.ts
+++ src/charts/index.ts
@@ -1,6 +1,6 @@
 import { IRequestContext } from '@/api/middleware';
 
-export const font = `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`;
+export const font = `Roboto`;
 
 export interface IVegaOptions {
   scaleFactor: number;
