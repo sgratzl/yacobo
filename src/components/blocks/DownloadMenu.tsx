f512ccf666ca5cce643a9434b21d8862465096c2
Samuel Gratzl <sam@sgratzl.com>
2020-09-17 12:00:45 +0200

    better download menu

--- src/components/blocks/DownloadMenu.tsx
+++ src/components/blocks/DownloadMenu.tsx
@@ -6,35 +6,34 @@ import {
   FileExcelOutlined,
   FilePdfOutlined,
 } from '@ant-design/icons';
-import { Fragment } from 'react';
 
 export function DownloadMenu({ path, img = true }: { path: string; img?: boolean }) {
   const menu = (
     <Menu>
       {img && (
-        <Fragment key="img">
-          (
-          <Menu.Item key="svg" icon={<FileImageOutlined />}>
+        <Menu.ItemGroup title="Image">
+          <Menu.Item icon={<FileImageOutlined />}>
             <a href={`/api${path}.svg?download&details`}>Download SVG</a>
           </Menu.Item>
-          <Menu.Item key="png" icon={<FileImageOutlined />}>
+          <Menu.Item icon={<FileImageOutlined />}>
             <a href={`/api${path}.png?download&details`}>Download PNG</a>
           </Menu.Item>
-          <Menu.Item key="pdf" icon={<FilePdfOutlined />}>
+          <Menu.Item icon={<FilePdfOutlined />}>
             <a href={`/api${path}.pdf?download&details`}>Download PDF</a>
           </Menu.Item>
-          <Menu.Item key="vg" icon={<FileImageOutlined />}>
+          <Menu.Item icon={<FileOutlined />}>
             <a href={`/api${path}.vg?download&details`}>Download Vega Spec</a>
           </Menu.Item>
-          )
-        </Fragment>
+        </Menu.ItemGroup>
       )}
-      <Menu.Item key="json" icon={<FileOutlined />}>
-        <a href={`/api${path}.json?download&details`}>Download JSON</a>
-      </Menu.Item>
-      <Menu.Item key="csv" icon={<FileExcelOutlined />}>
-        <a href={`/api${path}.csv?download&details`}>Download CSV</a>
-      </Menu.Item>
+      <Menu.ItemGroup title="Data">
+        <Menu.Item icon={<FileOutlined />}>
+          <a href={`/api${path}.json?download&details`}>Download JSON</a>
+        </Menu.Item>
+        <Menu.Item icon={<FileExcelOutlined />}>
+          <a href={`/api${path}.csv?download&details`}>Download CSV</a>
+        </Menu.Item>
+      </Menu.ItemGroup>
     </Menu>
   );
   return (
