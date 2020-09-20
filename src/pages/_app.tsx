e1e8dd112b2feeb717d8f8dc829a4b604818fec7
Samuel Gratzl <sam@sgratzl.com>
2020-09-20 15:48:33 +0200

    feat: start with openapi spec

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../client/globals.css';
+import 'swagger-ui-react/swagger-ui.css';
 
 function YaCoBo({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
---
+++ src/pages/openapi.tsx
@@ -0,0 +1,5 @@
+import SwaggerUI from 'swagger-ui-react';
+
+export default function OpenAPI() {
+  return <SwaggerUI url="./openapi.json" />;
+}
