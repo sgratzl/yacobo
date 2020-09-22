b99cb46d6e83915dcee68fd10008e153483d7001
Samuel Gratzl <sam@sgratzl.com>
2020-09-22 13:59:48 +0200

    test dynamic again for proper social media template

--- src/pages/compare/[regions]/date/[date].tsx
+++ src/pages/compare/[regions]/date/[date].tsx
@@ -1,10 +1,33 @@
-import { useQueryParam } from '@/client/hooks';
+import { useFallback } from '@/client/hooks';
 import { extractDate, extractRegions } from '@/common/validator';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 import { RegionsCompareOverview } from '@/components/pages/RegionsCompareOverview';
 
-export default function RegionSignalDateWrapper() {
-  const regions = useQueryParam(extractRegions, []);
-  const date = useQueryParam(extractDate);
+interface IRegionsDateProps {
+  regions: string;
+  date: string;
+}
+
+export const getStaticProps: GetStaticProps<IRegionsDateProps> = async (context) => {
+  return {
+    props: {
+      regions: context.params!.regions as string,
+      date: context.params!.date as string,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths<IRegionsDateProps & ParsedUrlQuery> = async () => {
+  return {
+    paths: [], // no favorite regions yet
+    fallback: true,
+  };
+};
+
+export default function RegionSignalDateWrapper(props: IRegionsDateProps) {
+  const regions = useFallback(props.regions, extractRegions, []);
+  const date = useFallback(props.date, extractDate, undefined);
 
   return <RegionsCompareOverview regions={regions} date={date} />;
 }
