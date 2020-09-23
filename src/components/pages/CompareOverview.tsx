71574b256ceb752c8d7cdab2c09e971281dbcefe
Samuel Gratzl <sam@sgratzl.com>
2020-09-23 09:42:51 +0200

    feat(compare): add examples to also prefetch them

--- src/components/pages/CompareOverview.tsx
+++ src/components/pages/CompareOverview.tsx
@@ -1,12 +1,16 @@
 import { formatAPIDate, formatLocal } from '@/common';
 import BaseLayout from '@/components/blocks/BaseLayout';
 import { DateSelect } from '@/components/blocks/DateSelect';
-import type { IRegion } from '@/model';
-import { Typography } from 'antd';
+import { IRegion, regionByID } from '@/model';
+import { Typography, List } from 'antd';
 import { Comparing } from '../blocks/Comparing';
+import LinkWrapper from '../blocks/LinkWrapper';
 import { RegionsSelect } from '../blocks/RegionSelect';
 
 const EMPTY: IRegion[] = [];
+
+const California_NewYork_Florida = [regionByID('06'), regionByID('36'), regionByID('12')];
+const AlleghenyCounty_Pennsylvania = [regionByID('42003'), regionByID('42')];
 
 export function CompareOverview({ date, dynamic }: { date?: Date; dynamic?: boolean }) {
   return (
@@ -39,6 +43,27 @@ export function CompareOverview({ date, dynamic }: { date?: Date; dynamic?: bool
     >
       <Typography.Title>Compare Regions</Typography.Title>
       <Typography.Paragraph>select one or more regions to start</Typography.Paragraph>
+      <Typography.Title level={2}>Examples</Typography.Title>
+      <List size="small">
+        <List.Item>
+          <LinkWrapper
+            passHref
+            path="/compare/[regions]/date/[date]"
+            query={{ date, regions: AlleghenyCounty_Pennsylvania }}
+          >
+            Allegheny County vs. Pennsylvania
+          </LinkWrapper>
+        </List.Item>
+        <List.Item>
+          <LinkWrapper
+            passHref
+            path="/compare/[regions]/date/[date]"
+            query={{ date, regions: California_NewYork_Florida }}
+          >
+            California vs. New York vs. Florida
+          </LinkWrapper>
+        </List.Item>
+      </List>
       <Comparing regions={EMPTY} path="/compare/[regions]/date/[date]" clearPath="/date/[date]" query={{ date }} />
     </BaseLayout>
   );
