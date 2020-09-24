5206b97d6fac67864422acf81ad7f1147a234c0e
Samuel Gratzl <sam@sgratzl.com>
2020-09-24 14:05:15 +0200

    try to fix url redirect

--- src/api/redirect.ts
+++ src/api/redirect.ts
@@ -1,10 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export function redirectWithFormat(req: NextApiRequest, res: NextApiResponse, suffix: string) {
-  const url = req.url!;
-  const format = url.lastIndexOf('.');
-  console.error(url, format);
+  const url = req.headers.location ?? req.url!;
+  const query = url.indexOf('?');
+  const format = query >= 0 ? url.slice(0, query).lastIndexOf('.') : url.lastIndexOf('.');
+  console.error(url, format, query);
   if (format < 0) {
+    if (query >= 0) {
+      return res.redirect(`${url.slice(0, query)}/${suffix}${url.slice(query)}`);
+    }
     return res.redirect(`${url}/${suffix}`);
   }
   console.error(`${url.slice(0, format)}/${suffix}${url.slice(format)}`);
