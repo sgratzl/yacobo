595ff5add6b351dad44463a384242b337ae988e6
Samuel Gratzl <sam@sgratzl.com>
2020-09-24 14:12:41 +0200

    remove log statements

--- src/api/redirect.ts
+++ src/api/redirect.ts
@@ -4,13 +4,11 @@ export function redirectWithFormat(req: NextApiRequest, res: NextApiResponse, su
   const url = req.headers.location ?? req.url!;
   const query = url.indexOf('?');
   const format = query >= 0 ? url.slice(0, query).lastIndexOf('.') : url.lastIndexOf('.');
-  console.error(url, format, query);
   if (format < 0) {
     if (query >= 0) {
       return res.redirect(`${url.slice(0, query)}/${suffix}${url.slice(query)}`);
     }
     return res.redirect(`${url}/${suffix}`);
   }
-  console.error(`${url.slice(0, format)}/${suffix}${url.slice(format)}`);
   return res.redirect(`${url.slice(0, format)}/${suffix}${url.slice(format)}`);
 }
